6b09517f9e73e5aca0f2497836ddeb9b92caf27b
garg-anuj <anujgarg2018@gmail.com>
2025-03-16 12:32:58 +0530

    correction in footer style

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import "./footer.css";
 
 const Footer = () => {
   return (
-    <footer className="footer position-absolute bottom-0 w-100 bg-light p-2">
+    <footer className="footer position-relative top-100 w-100 bg-light p-2">
       <div className="border p-2">Footer</div>
     </footer>
   );

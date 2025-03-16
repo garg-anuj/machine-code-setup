84df9d164af6602ac1dfd8876fa137d5d75c6b0f
garg-anuj <anujgarg2018@gmail.com>
2025-03-16 14:01:36 +0530

    freeze object explanation

--- src/config/constantObj.js
+++ src/config/constantObj.js
@@ -1,2 +1,5 @@
+// it will help  to avoid unnecessary re-creations for better optimizing performance it is helpful
+// when our component re-render that time object and array crate its instances so to avoid
+//  this we use these freeze objects
 export const EMPTY_ARRAY = Object.freeze([]);
 export const EMPTY_OBJECT = Object.freeze({});
